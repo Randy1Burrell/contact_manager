eeca55c8478dfe044784c1f36786451b6d2e7f55
Randy1Burrell <Randy1burrell@yahoo.com>
2018-01-06 19:42:43 -0500

    Created file to hold contact schema

---
+++ app_api/models/contacts.js
@@ -0,0 +1,13 @@
+/**
+ * Author: Randy Burrell
+ *
+ * Date Created: 2018-01-06 YYYY-MM-DD
+ *
+ * Description: this file is the contains
+ * schema for contacts
+ */
+
+/**
+ * Getting required depandencies
+ */
+var mongoose = require('mongoose');
