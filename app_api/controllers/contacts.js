958372e764ae22ef5db76c3bd6374ffe9d136b89
Randy1Burrell <Randy1burrell@yahoo.com>
2018-01-06 19:45:18 -0500

    Controller file for contacts with some dummy data

--- app_api/controllers/contacts.js
+++ app_api/controllers/contacts.js
@@ -2,13 +2,28 @@
  * Require dependencies
  */
 var mongoose = require('mongoose');
-var contact = mongoose.model('Contact');
+//var contact = mongoose.model('Contact');
 
 /**
  * Function used for sending responses in JSON
  * format
  */
-var sendJsonResponse = funrtion(res, status, content) {
+var sendJsonResponse = function(res, status, content) {
   res.status(status);
   res.json(content);
 };
+
+/**
+ * Dummy data for testing
+ */
+var respObj = {
+  name: "Randy",
+  age: 33,
+  address: "kng 6"
+}
+/**
+ * Controller for getting contacts
+ */
+module.exports.getContactList = function(req, res) {
+  sendJsonResponse(res, 200, respObj);
+}
