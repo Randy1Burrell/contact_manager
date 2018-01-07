a25942f20bb0e272d4122992ccd86eb0310fa830
Randy1Burrell <Randy1burrell@yahoo.com>
2018-01-06 22:11:27 -0500

    Included database model into controller

--- app_api/controllers/contacts.js
+++ app_api/controllers/contacts.js
@@ -2,44 +2,44 @@
  * Require dependencies
  */
 var mongoose = require('mongoose');
-//var contact = mongoose.model('Contact');
+var contact = mongoose.model('Contact');
 
 /**
  * Function used for sending responses in JSON
  * format
  */
 var sendJsonResponse = function(res, status, content) {
-    res.status(status);
-    res.json(content);
+  res.status(status);
+  res.json(content);
 };
 
 /**
  * Dummy data for testing
  */
 var respObj = {
-    name: "Randy",
-    age: 33,
-    address: "kng 6"
+  name: "Randy",
+  age: 33,
+  address: "kng 6"
 }
 /**
  * Controller for getting contacts
  */
 module.exports.getContactList = function(req, res) {
-    sendJsonResponse(res, 200, respObj);
+  sendJsonResponse(res, 200, respObj);
 }
 
 module.exports.createContact = function(req, res) {
-    sendJsonResponse(res, 200, respObj);
+  sendJsonResponse(res, 200, respObj);
 }
 
 module.exports.getContactInfo = function(req, res) {
-    sendJsonResponse(res, 200, respObj);
+  sendJsonResponse(res, 200, respObj);
 }
 
 module.exports.updateContactInfo = function(req, res) {
-    sendJsonResponse(res, 200, respObj);
+  sendJsonResponse(res, 200, respObj);
 }
 
 module.exports.deleteContact = function(req, res) {
-    sendJsonResponse(res, 200, respObj);
+  sendJsonResponse(res, 200, respObj);
 }
