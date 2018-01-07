a30744bb1963c79431cc0dcf305ff6d36f4ad4f9
Randy1Burrell <Randy1burrell@yahoo.com>
2018-01-06 21:44:36 -0500

    Create schema for contact called contactSchema

--- app_api/models/contacts.js
+++ app_api/models/contacts.js
@@ -11,3 +11,37 @@
  * Getting required depandencies
  */
 var mongoose = require('mongoose');
+
+/**
+ * Create new schema for contact
+ * called contactSchema
+ */
+var contactSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  dob: {
+    type: Date,
+  },
+  address: [{
+    type: String
+  }],
+  phoneNumber: {
+    type: [String],
+    required: true
+  },
+  email: {
+    type: [String],
+    required: true
+  },
+});
+
+/**
+ * Compile contactSchema
+ */
+mongoose.model('Contact', contactSchema);
