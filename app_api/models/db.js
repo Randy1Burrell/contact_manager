d86fcdb7c28f3cda3d2ee906dd9098aabece12d2
Randy1Burrell <Randy1burrell@yahoo.com>
2018-01-07 15:38:19 -0500

    Better control with nodemon for testing

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -27,20 +27,20 @@ var dbURI = 'mongodb://localhost/contacts';
  * for example NODE_ENV = production
  */
 if (process.env.NODE_ENV === 'production') {
-    if (process.env.MONGOLAB_URI) {
-        /**
-         * Setting dbURI to connection string
-         * that should be in .env files key
-         * value pair. MONGOLAB_URI = URI
-         */
-        dbURI = process.env.MONGOLAB_URI;
-    } else {
-        /**
-         * If MONGOLAB_URI was not set in .env
-         * file then revert to default URI
-         */
-        dbURI = 'mongodb://localhost/contacts';
-    }
+  if (process.env.MONGOLAB_URI) {
+    /**
+     * Setting dbURI to connection string
+     * that should be in .env files key
+     * value pair. MONGOLAB_URI = URI
+     */
+    dbURI = process.env.MONGOLAB_URI;
+  } else {
+    /**
+     * If MONGOLAB_URI was not set in .env
+     * file then revert to default URI
+     */
+    dbURI = 'mongodb://localhost/contacts';
+  }
 }
 
 /**
@@ -54,7 +54,62 @@ mongoose.connect(dbURI);
  * has been made
  */
 mongoose.connection.on('connected', function() {
-    console.log('Mongoose connected to ' + dbURI);
+  console.log('Mongoose connected to ' + dbURI);
+});
+
+/**
+ * Listen for connection error when connecting/connected
+ * to mongodb and logs error to console
+ */
+mongoose.connection.on('error', function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+
+/**
+ * Listen for disconnection from mongodb and logs
+ * message to console
+ */
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
+
+/**
+ * Used to close connections to mongodb
+ */
+var gracefulShutdown = function (msg, callback) {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected through ' + msg);
+    process.exit(0);
+  });
+};
+
+/**
+ * Used to close connections to mongodb when nodemon
+ * restarts
+ */
+process.on('SIGUSR2', function () {
+  gracefulShutdown('nodemon restart', function () {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+
+/**
+ * Used to close connections to mongodb when app
+ * terminates
+ */
+process.on('SIGINT', function () {
+  gracefulShutdown('app termination', function () {
+    process.exit(0);
+  });
+});
+/**
+ * Used to close connections to mongodb when Heroku app
+ * shutsdown
+ */
+process.on('SIGTERM', function () {
+  gracefulShutdown('Heroku app shutdown', function () {
+    process.exit(0);
+  });
 });
 
 /**
