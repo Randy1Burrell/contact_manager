171cc00a56fc5b813fd05e274f6883a14fc342ca
Randy1Burrell <Randy1burrell@yahoo.com>
2018-01-09 20:00:15 -0500

    Importing newcomponents

--- app_client/src/App.vue
+++ app_client/src/App.vue
@@ -1,45 +1,62 @@
 <template>
   <div id="app">
-    <h1>Hello world!</h1>
+    <app-navbar></app-navbar>
+    <app-side-bar></app-side-bar>
+    <div class="col-md-4" v-for="contact in  contacts">
+      <app-contacts :contact="contact"></app-contacts>
+    </div>
+    <a class="add-contact">
+      <i class="fa fa-plus" aria-hidden="true"></i>
+    </a>
   </div>
 </template>
 
 <script>
+import Contacts from './components/Contacts.vue'
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/Sidebar.vue'
 export default {
+  components: {
+    'app-contacts': Contacts,
+    'app-navbar': Navbar,
+    'app-sidebar': Sidebar
+  },
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Contacts List",
+      contacts: []
     }
+  },
+  created() {
+    this.$http.get('http://localhost:3000/api/contact/').then((data) => {
+      this.contacts = data.body;
+    });
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+body {
+  padding-top: 60px;
+}
+h1 {
+  max-width: 200px;
+  margin: auto;
+  font-size: 10px;
+}
+.add-contact{
+  border: 1px solid #09cfc0;
+  padding: 12px 23px;
+  color: #fff !important;
+  font-weight: bolder;
+  background-color: #09cfc0;
+  position: absolute;
+  bottom: 40px;
+  right: 40px;
+  line-height: 2;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+  padding-top: 30px;
 }
 </style>
