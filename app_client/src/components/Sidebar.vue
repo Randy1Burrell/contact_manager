465dba346a638420d555cbbed545d1da036242e0
Randy1Burrell <Randy1burrell@yahoo.com>
2018-01-11 13:45:00 -0500

    Created sidebar that listens for even to show and hide

--- app_client/src/components/Sidebar.vue
+++ app_client/src/components/Sidebar.vue
@@ -1,22 +1,122 @@
 <template>
-  <div id="side" class="col-md-3 col-sm-8">
-    <ul>
-      <li></li>
-    </ul>
+  <div id="mySidenav" v-bind:class="{sidenav: true, 'open-nav': !close, 'close-nav': close}">
+    <a href="javascript:void(0)" class="closebtn" @click.prevent="closeNav()">&times;</a>
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Clients</a>
+    <a href="#">Contact</a>
   </div>
 </template>
 
 <script>
+import {sideNav} from '../bus/navigation'
+
 export default {
   name: 'Sidebar',
   data () {
     return {
-      view: false
+      close: open,
     }
+  },
+  methods: {
+    closeNav : () => {
+      sideNav.$emit("closeNav", true);
+    },
+  },
+  props: {
+    open: {
+      type: Boolean,
+      required: true
+    }
+  },
+  created() {
+    sideNav.$on("openNav", (event) => {
+      this.close = !event;
+    });
+    sideNav.$on("closeNav", (event) => {
+      this.close = event;
+    });
   }
 }
+
 </script>
 
 <style lang="scss" scoped>
-</style>
+/**
+ * The side navigation menu
+ */
+.sidenav {
+  height           : 100%; /* 100% Full-height */
+  width            : 0; /* 0 width - change this with JavaScript */
+  position         : fixed; /* Stay in place */
+  z-index          : 1; /* Stay on top */
+  top              : 0; /* Stay at the top */
+  left             : 0;
+  background-color : #111; /* Black*/
+  overflow-x       : hidden; /* Disable horizontal scroll */
+  padding-top      : 60px; /* Place content 60px from the top */
+  transition       : 0.5s; /* 0.5 second transition effect to slide in the sidenav */
+}
 
+/* The navigation menu links */
+.sidenav a {
+  padding         : 8px 8px 8px 32px;
+  text-decoration : none;
+  font-size       : 25px;
+  color           : #818181;
+  display         : block;
+  transition      : 0.3s;
+}
+
+/* When you mouse over the navigation links, change their color */
+.sidenav a:hover {
+  color : #f1f1f1;
+}
+
+/* Position and style the close button (top right corner) */
+.sidenav .closebtn {
+  position    : absolute;
+  top         : 0;
+  right       : 25px;
+  font-size   : 36px;
+  margin-left : 50px;
+}
+
+/**
+ *
+ */
+.close-nav {
+  width: 0px;
+}
+
+/**
+ *
+ */
+.open-nav {
+  width: 250px;
+}
+
+/* Style page content - use this if you want to push the page content to the right when you open the side navigation */
+#main {
+  transition : margin-left .5s;
+  padding    : 20px;
+}
+
+/* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top : 15px;}
+  .sidenav a {font-size : 18px;}
+}
+.closebtn {
+  padding-left : 10px;
+  color        : #888;
+  font-weight  : bold;
+  float        : right;
+  font-size    : 20px;
+  cursor       : pointer;
+}
+
+.closebtn:hover {
+  color : #000;
+}
+</style>
