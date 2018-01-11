149716dac31a884f25d5997e47bf018f810688df
Randy1Burrell <Randy1burrell@yahoo.com>
2018-01-10 23:49:00 -0500

    Made comments, fixed some functionality and cleaned up code

--- app_client/src/components/Navbar.vue
+++ app_client/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="topnav" id="myTopnav">
     <a href="#home" class="active">&#9776;</a>
     <a>Contact Manager</a>
-    <a class="">
+    <a>
       <div class="input-group">
         <span class="input-group-btn">
           <button class="btn btn-default" type="button">
@@ -11,6 +11,9 @@
         </span>
         <input type="text" class="form-control" placeholder="Search Contacts">
       </div>
+    </a>
+    <a href="javascript:void(0);" class="" @click="">
+      <i class="fa fa-plus-circle" aria-hidden="true"></i>
     </a>
     <a href="javascript:void(0);" class="active icon" @click="openSearch()">
       <i class="fa fa-search" aria-hidden="true"></i>
@@ -48,33 +51,34 @@ export default {
 /**
  * Style for search bar
  */
-$green : #4caf50;
+$green            : #4caf50;
+$hover-background : #ddd;
 
 button {
-  color: $green;
-  background-color: #fff;
-  border: 0px;
+  color            : $green;
+  background-color : #fff;
+  border           : 0px;
 }
 
 a .input-group input {
-  padding-bottom: 3px;
-  color: #4caf50;
-  padding-top: 3px;
-  border: 0px;
+  padding-bottom : 3px;
+  color          : $green;
+  padding-top    : 3px;
+  border         : 0px;
 }
 
 *:focus {
-  outline: none !important;
-  -webkit-appearance:none;
-  box-shadow: 0px 0px;
+  outline            : none !important;
+  -webkit-appearance : none;
+  box-shadow         : 0px 0px;
 }
 
 /**
  * Add a black background color to the top navigation
  */
 .topnav {
-  background-color: #333;
-  overflow: hidden;
+  background-color : #333;
+  overflow         : hidden;
 }
 
 /**
@@ -84,7 +88,7 @@ a .input-group input {
   font-variant    : all-petite-caps;
 }
 .topnav a {
-  color           : #f2f2f2;
+  color           : $green;
   display         : block;
   float           : left;
   font-size       : 17px;
@@ -98,7 +102,7 @@ a .input-group input {
  */
 .topnav a:hover {
   background-color : #ddd;
-  color            : $green;
+  color            : $green !important;
 }
 
 .topnav a:nth-child(3) {
@@ -117,8 +121,8 @@ a .input-group input {
  * the hamburger menu
  */
 .active {
-  background-color : #4CAF50;
-  color            : white;
+  background-color : $green;
+  color            : white !important;
 }
 
 /**
@@ -137,28 +141,74 @@ a .input-group input {
  * (.icon)
  */
 @media screen and (max-width: 600px) {
-  .topnav a:not(:first-child) {
-    display : none;
-  }
-  .topnav a:nth-child(2) {
-    display    : inline-block;
-    min-width  : 220px;
-    max-width  : 350px;
-    text-align : center;
-  }
-  .topnav a.icon {
-    float   : right;
-    display : block;
-  }
-
   /**
-   * The "responsive" class is added
-   * to the topnav with JavaScript when
-   * the user clicks on the icon. This
-   * class makes the topnav look good on
-   * small screens (display the links
-   * vertically instead of horizontally)
+   * Default setting for text box
    */
+input[type="text"] {
+  max-width : 500px !important;
+  min-width : 250px !important;
+}
+
+/**
+ * Remove input button on small scrrens
+ */
+.input-group-btn{
+  display : none;
+}
+
+/**
+ * Remove all anchor tags except the first one
+ */
+.topnav a:not(:first-child) {
+  display : none;
+}
+
+/**
+ * Now show the second anchor tag
+ */
+.topnav a:nth-child(2) {
+  display    : inline-block;
+  min-width  : 229px;
+  max-width  : 450px;
+  text-align : center;
+}
+
+/**
+ * Keep current background-color
+ */
+.topnav a:nth-child(2):hover{
+  background-color : #333;
+}
+/**
+ * Give search bar hover background
+ * and remove the padding-left;
+ */
+.topnav a:nth-child(3) {
+  background-color : $hover-background;
+  padding-left     : 0px;
+}
+/**
+ * Remove the fourth anchor
+ */
+.topnav a:nth-child(4) {
+  display : none !important;
+}
+/**
+ * Keep search icon to the right
+ */
+.topnav a.icon {
+  float   : right;
+  display : block;
+}
+
+/**
+ * The "responsive" class is added
+ * to the topnav with JavaScript when
+ * the user clicks on the icon. This
+ * class makes the topnav look good on
+ * small screens (display the links
+ * vertically instead of horizontally)
+ */
 .topnav.responsive {position: relative;}
 .topnav.responsive a.icon {
   position : absolute;
@@ -166,9 +216,7 @@ a .input-group input {
   top      : 0;
 }
 .topnav.responsive a {
-  float      : none;
-  display    : block;
-  text-align : left;
+  display    : inline-block;
 }
 }
 </style>
