f530129f3b033106ff05f0e1591b855db36aa2dc
Randy1Burrell <Randy1burrell@yahoo.com>
2018-01-11 23:06:56 -0500

    Remove statement requiring jquery

--- app_client/src/main.js
+++ app_client/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
-require('jquery')
 import VueResource from 'vue-resource'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
