4d404af41a23623797a75f2798b86dafcb309ba8
Randy1Burrell <Randy1burrell@yahoo.com>
2018-01-15 15:02:37 -0500

    Contacts now displays a contact rather than to loop over many contacts. This was done to be able to seperate different contact details.

--- app_client/src/components/Contacts.vue
+++ app_client/src/components/Contacts.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-  <div class="chip" v-for="i in 3000">
-    <img src="https://www.randyburrell.info/wp-content/uploads/2017/05/Randy_Burrell_Graduation_Picture.jpg" alt="Person" width="96" height="96">
-    John Doe
-    <span class="closebtn" onclick="this.parentElement.style.display='none'">&times;</span>
-  </div>
+  <div @click="viewContact" :class="{'container-fluid': true, shift: openNav, shiftSearch: search}">
+    <div class="chip">
+      <img src="https://www.randyburrell.info/wp-content/uploads/2017/05/Randy_Burrell_Graduation_Picture.jpg" alt="Person" width="96" height="96">
+      <ul>
+        <li> {{ contact.firstname }} {{ contact.lastname }} </li>
+        <li> {{ contact.email[0] }} </li>
+        <li> {{ contact.phoneNumber[0] }} </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -13,35 +16,91 @@ import {sideNav} from '../bus/navigation'
 
 export default {
   name: 'Contacts',
-  props: {
-    contact: {
-      type: Object,
-      //required: true
-    }
-  },
   data () {
     return {
-      view: false
     }
   },
-  created () {
-    sideNav.$on("openNav", (event) => {
-      this.small = event;
-    });
+  methods: {
+    viewContact: function() {
+      sideNav.$emit("viewContact", this.contact);
+    }
+  },
+  props: {
+    openNav: {
+      type: Boolean
+    },
+    contact: {
+      type: Object
+    },
+    search: {
+      type: Boolean
+    }
+  },
+  created: function () {
   }
 }
 </script>
 
 <style lang="scss" scoped>
-a {
-  float: right;
-  font-size: 13px;
-  padding: 3px 11px;
-  color: #09cfc0;
-  margin-left:4px
+div {
+  .chip {
+    display          : block;
+    margin           : 5px 15px;
+    padding          : 0px 25px;
+    height           : 50px;
+    font-size        : 14px;
+    line-height      : 50px;
+    border-radius    : 5px;
+    background-color : #fff;
+    min-width        : 60%;
+    max-width        : 100%;
+  }
+
+  .chip img {
+    float            : left;
+    margin           : 0 10px 0 -25px;
+    height           : 50px;
+    width            : 50px;
+    border-radius    : 50px;
+    background-color : red;
+  }
+
+  .chip:hover {
+    background-color : #ddd;
+    cursor           : pointer;
+  }
+
+  .chip ul {
+    overflow-x : hidden;
+  }
+  .chip ul li {
+    display    : inline-block;
+    overflow-x : hidden;
+    width      : 300px;
+  }
 }
-.container{
-  font-size: 14px;
+
+.container-fluid {
+  margin-left : 0px;
+  font-size   : 14px;
+  transition         : all 0.5s linear !important;
+  -webkit-transition : all 0.5s linear !important;
+}
+.container-fluid:first-child {
+  padding-top : 70px !important;
+}
+
+.shift {
+  margin-left: 200px !important;
+}
+
+@media screen and (max-width: 991px) {
+  .shift {
+    margin-left : 0px !important;
+  }
+  .shiftSearch {
+    padding-top: 100px !important;
+  }
 }
 </style>
 
