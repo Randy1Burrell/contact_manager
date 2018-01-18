63efebefb814e5e470b88c1bdf8cab59f90d2f13
Randy1Burrell <Randy1burrell@yahoo.com>
2018-01-17 23:34:47 -0500

    Make sure all letters are capitalized before returned from the filter

--- app_client/src/components/Contacts.vue
+++ app_client/src/components/Contacts.vue
@@ -47,7 +47,7 @@ export default {
      * Get the first letter of a string
      */
     getLetter(value) {
-      return value.slice(0, 1);
+      return value.slice(0, 1).toUpperCase();
     }
   },
   methods: {
