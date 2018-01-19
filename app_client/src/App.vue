fc18dfc167a5cb4b98e42275c6a04578a3331796
Randy1Burrell <Randy1burrell@yahoo.com>
2018-01-19 18:22:03 -0500

    Code clean up

--- app_client/src/App.vue
+++ app_client/src/App.vue
@@ -304,18 +304,18 @@ export default {
  * of the screen
  */
 #snackbar {
-  visibility: hidden;
-  min-width: 250px;
-  margin-left: -125px;
-  background-color: #333;
-  color: #fff;
-  text-align: center;
-  border-radius: 2px;
-  padding: 16px;
-  position: fixed;
-  z-index: 1;
-  left: 50%;
-  bottom: 30px;
+  visibility       : hidden;
+  min-width        : 250px;
+  margin-left      : -125px;
+  background-color : #333;
+  color            : #fff;
+  text-align       : center;
+  border-radius    : 2px;
+  padding          : 16px;
+  position         : fixed;
+  z-index          : 1;
+  left             : 50%;
+  bottom           : 30px;
 }
 
 /**
@@ -323,9 +323,9 @@ export default {
  * on a button (class added with JavaScript)
  */
 #snackbar.show {
-  visibility: visible;
-  -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
-  animation: fadein 0.5s, fadeout 0.5s 2.5s;
+  visibility        : visible;
+  -webkit-animation : fadein 0.5s, fadeout 0.5s 2.5s;
+  animation         : fadein 0.5s, fadeout 0.5s 2.5s;
 }
 
 /**
@@ -333,22 +333,46 @@ export default {
  * in and out
  */
 @-webkit-keyframes fadein {
-  from {bottom: 0; opacity: 0;}
-  to {bottom: 30px; opacity: 1;}
+  from {
+    bottom  : 0;
+    opacity : 0;
+  }
+  to {
+    bottom  : 30px;
+    opacity : 1;
+  }
 }
 
 @keyframes fadein {
-  from {bottom: 0; opacity: 0;}
-  to {bottom: 30px; opacity: 1;}
+  from {
+    bottom  : 0;
+    opacity : 0;
+  }
+  to {
+    bottom  : 30px;
+    opacity : 1;
+  }
 }
 
 @-webkit-keyframes fadeout {
-  from {bottom: 30px; opacity: 1;}
-  to {bottom: 0; opacity: 0;}
+  from {
+    bottom  : 30px;
+    opacity : 1;
+  }
+  to {
+    bottom  : 0;
+    opacity : 0;
+  }
 }
 
 @keyframes fadeout {
-  from {bottom: 30px; opacity: 1;}
-  to {bottom: 0; opacity: 0;}
+  from {
+    bottom  : 30px;
+    opacity : 1;
+  }
+  to {
+    bottom  : 0;
+    opacity : 0;
+  }
 }
 </style>
