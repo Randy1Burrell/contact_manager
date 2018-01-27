be3801bb1aeec2e12e13bfff67a65897a49e52c0
Randy1Burrell <Randy1burrell@yahoo.com>
2018-01-26 23:49:48 -0500

    Config update

--- app_client/src/config.js
+++ app_client/src/config.js
@@ -1,5 +1,5 @@
 var get_api = function () {
-  if (process.env.NODE_ENV === "Production" ) {
+  if (process.env.NODE_ENV === "production" ) {
     return 'https://calm-harbor-49946.herokuapp.com/api/contact/';
   } else {
     return 'http://localhost:3000/api/contact/';
